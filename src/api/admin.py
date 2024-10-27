f9bf986e705ad6a9cc858a9b263df1e014db8ecb
AntonyAPT <antony.tartakovskiy@gmail.com>
2024-10-26 21:55:14 -0700

    added admin.py

---
+++ src/api/admin.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+@router.post("/reset")
+def reset():
+    """
+   Resets all chores
+    """
+    return "OK"
