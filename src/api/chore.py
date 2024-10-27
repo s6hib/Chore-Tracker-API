f8c2d0638903ddd86e83aaef820cb29d1031d0b2
colander21 <142125956+colander21@users.noreply.github.com>
2024-10-26 22:12:04 -0700

    Update chore.py with fast api imports

--- src/api/chore.py
+++ src/api/chore.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter, Depends
+from src.api import auth
 
+router = APIRouter(
+    prefix="/bottler",
+    tags=["bottler"],
+    dependencies=[Depends(auth.get_api_key)],
+)
