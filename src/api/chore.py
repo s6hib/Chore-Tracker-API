17b7dba85f5cab1c99200013b253b2311b08888d
colander21 <carsonolander10@gmail.com>
2024-10-27 21:27:08 -0700

    add chore_assignment api

--- src/api/chore.py
+++ src/api/chore.py
@@ -1,7 +1,19 @@
-import sqlalchemy
-from src import database as db
 from fastapi import APIRouter, Depends
 from src.api import auth
+from pydantic import BaseModel
+import datetime
+
+import sqlalchemy
+from src import database as db
+
+
+class Chore(BaseModel):
+    name: str
+    location_in_house: str
+    frequency: str
+    duration_mins: int
+    priority: int
+    due_date: datetime.datetime
 
 router = APIRouter(
     prefix="/chore",
--- src/api/chore_assignment.py
+++ src/api/chore_assignment.py
@@ -1,8 +1,61 @@
 from fastapi import APIRouter, Depends
 from src.api import auth
+from roommate import Roommate
+from chore import Chore
+
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/chore_assignment",
     tags=["chore_assignment"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+@router.post("/chores/", tags=["chore"])
+def assign_chore(chore_to_assign: Chore, roommate_to_assign: Roommate):
+    with db.engine.begin() as connection:
+        chore_id = connection.execute(sqlalchemy.text(
+            '''SELECT id 
+            FROM chores 
+            WHERE name = :chore_to_assign_name, 
+                location_in_house = :chore_to_assign_location, 
+                frequency = :chore_to_assign_frequency,
+                duration_mins = :chore_to_assign_duration,
+                priority = :chore_to_assign_priority'''
+                ),
+                {
+                "chore_to_assign_name": chore_to_assign.name,
+                "chore_to_assign_location": chore_to_assign.location_in_house,
+                "chore_to_assign_frequency": chore_to_assign.frequency,
+                "chore_to_assign_duration": chore_to_assign.duration_mins,
+                "chore_to_assign_priority": chore_to_assign.priority
+                }
+        ).fetchone()
+        roommate_id = connection.execute(sqlalchemy.text(
+            '''SELECT id 
+            FROM roommate 
+            WHERE first_name = :first_name,
+                last_name = :last_name,
+                email = :email'''
+                ),
+                {
+                "first_name": roommate_to_assign.first_name,
+                "last_name": roommate_to_assign.last_name,
+                "email": roommate_to_assign.email
+                }
+        ).fetchone()
+        
+        connection.execute(sqlalchemy.text(
+                """
+                INSERT INTO chore_assignment (chore_id, roommate_id, status) 
+                VALUES (:chore_id, :roommate_id, 'not_completed')
+                """
+            ),
+            {
+                "chore_id": chore_id,
+                "roommate_id": roommate_id
+            }
+        )
+        
+    return {"chore_id": chore_id, "roommate":roommate_id, "status": "not completed"}
