b7b178000d20e2c8faf4acf589730ac3636249ad
colander21 <142125956+colander21@users.noreply.github.com>
2024-10-27 19:51:25 -0700

    Update roommate.py with create_roommate api call

--- src/api/roommate.py
+++ src/api/roommate.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter, Depends
 from src.api import auth
+from pydantic import BaseModel
+
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/roommate",
@@ -7,7 +11,20 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
+class Roommate(BaseModel):
+    first_name: string
+    last_name: string
+    email: string
+
 @router.get("/roommates/", tags=["roommate"])
 def get_roommates():
     
     return "test"
+
+@router.post("/roommates/", tags=["roommate"])
+def create_roommate(new_roommate: Roommate):
+    print(new_roommate)
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("INSERT INTO roommate (first_name, last_name, email) VALUES (:first_name, :last_name, :email);" , [{"first_name": new_roommate.first_name}, {"last_name": new_roommate.last_name}, {"email": new_roommate.email}])
+        
+    return new_roommate
