d334376b982a8737daa7b1e641c2cf72f3a51f89
AntonyAPT <antony.tartakovskiy@gmail.com>
2024-11-03 20:06:23 -0800

    implemented get bill

--- src/api/bill.py
+++ src/api/bill.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
-from src.api.roommate import Roommate
-from src.api.chore import Chore
+import datetime
 
 import sqlalchemy
 from src import database as db
@@ -14,7 +13,35 @@ router = APIRouter(
 )
 
 class bill(BaseModel):
+    cost: float
+    due_date: datetime.date
     bill_type: str
-    due_date: str
+    message: str
     roommate: str
     status: str
+
+@router.get("/bills/", tags=["bill"])
+def get_bills():
+    with db.engine.begin() as connection:
+       result = connection.execute(sqlalchemy.text(
+            '''SELECT cost, due_date, bill_type, message,
+            b.roommate, b.status
+            FROM bill 
+            JOIN bill_list b ON b.bill_id = bill.id''')).fetchall() 
+       
+    bill_list = []
+            
+    for bill in result:
+        bill_list.append({
+            "cost": bill.cost,
+            "due_date": bill.due_date,
+            "bill_type": bill.bill_type,
+            "message": bill.message,
+            "roommate": bill.roommate,
+            "status": bill.status
+            })
+        print(bill)
+        
+    return bill_list
+
+
