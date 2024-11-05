0d669f3179c93592bc7aa26c84c0a4b4d93d034d
Sue Sue <29suesue@gmail.com>
2024-11-04 23:36:12 -0800

    fix typo

--- src/api/roommate.py
+++ src/api/roommate.py
@@ -16,12 +16,12 @@ class Roommate(BaseModel):
     last_name: str
     email: str
 
-@router.get("/get_roommates", tags=["roommate"])
+@router.get("/get_roommate", tags=["roommate"])
 def get_roommates():
     
     return "test"
 
-@router.post("/create_roommates", tags=["roommate"])
+@router.post("/create_roommate", tags=["roommate"])
 def create_roommate(new_roommate: Roommate):
     with db.engine.begin() as connection:
         roommate_id = connection.execute(
